ca3f9f925a6dcf86e2970026f873dd2f45cfdf08
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-07-21 23:09:55 -0700

    Migrate actual impl logic into the crate

--- fs-server/src/bin/run-server.rs
+++ fs-server/src/bin/run-server.rs
@@ -1,53 +1,14 @@
-use futures::{future, Future, Stream};
-use log::{error, trace, info};
+use futures::{Future, Stream};
+use log::{error, info};
 use tokio::net::TcpListener;
-use tower_grpc::{Request, Response};
 use tower_hyper::server::{Http, Server};
 
-pub mod proto {
-    include!(concat!(env!("OUT_DIR"), "/fs.rs"));
-}
-
-use crate::proto::server;
-use crate::proto::{GetRequest, GetResponse,
-                   ListRequest, ListResponse,
-                   WriteRequest, WriteResponse};
-
-#[derive(Clone, Debug)]
-struct FileSystemImpl;
-
-impl server::FileSystem for FileSystemImpl {
-    type GetFuture = future::FutureResult<Response<GetResponse>, tower_grpc::Status>;
-    type ListFuture = future::FutureResult<Response<ListResponse>, tower_grpc::Status>;
-    type WriteFuture = future::FutureResult<Response<WriteResponse>, tower_grpc::Status>;
-
-    fn get(&mut self, request: Request<GetRequest>) -> Self::GetFuture {
-      trace!("[GET] request = {:?}", request);
-
-        let response = Response::new(GetResponse {
-            content: b"Zomg, it works!".to_vec(),
-        });
-
-        future::ok(response)
-    }
-
-    fn list(&mut self, request: Request<ListRequest>) -> Self::ListFuture {
-      trace!("[LIST] request = {:?}", request);
-      unimplemented!()
-    }
-
-    fn write(&mut self, request: Request<WriteRequest>) -> Self::WriteFuture {
-      trace!("[WRITE] request = {:?}", request);
-      unimplemented!()
-    }
-}
+use fs_server::FileSystemImpl;
 
 pub fn main() {
     let _ = ::env_logger::init();
 
-    let new_service = server::FileSystemServer::new(FileSystemImpl);
-
-    let mut server = Server::new(new_service);
+    let mut server = Server::new(FileSystemImpl.into_service());
 
     let http = Http::new().http2_only(true).clone();
 
--- fs-server/src/lib.rs
+++ fs-server/src/lib.rs
@@ -1,1 +1,45 @@
-const PORT: u32 = 50051;
+use futures::future;
+use log::trace;
+use tower_grpc::{Request, Response};
+
+pub mod proto {
+    include!(concat!(env!("OUT_DIR"), "/fs.rs"));
+}
+
+use crate::proto::{
+    GetRequest, GetResponse, ListRequest, ListResponse, WriteRequest, WriteResponse,
+};
+
+#[derive(Clone, Debug)]
+pub struct FileSystemImpl;
+impl FileSystemImpl {
+    pub fn into_service(self) -> proto::server::FileSystemServer<Self> {
+        proto::server::FileSystemServer::new(self)
+    }
+}
+
+impl proto::server::FileSystem for FileSystemImpl {
+    type GetFuture = future::FutureResult<Response<GetResponse>, tower_grpc::Status>;
+    type ListFuture = future::FutureResult<Response<ListResponse>, tower_grpc::Status>;
+    type WriteFuture = future::FutureResult<Response<WriteResponse>, tower_grpc::Status>;
+
+    fn get(&mut self, request: Request<GetRequest>) -> Self::GetFuture {
+        trace!("[GET] request = {:?}", request);
+
+        let response = Response::new(GetResponse {
+            content: b"Zomg, it works!".to_vec(),
+        });
+
+        future::ok(response)
+    }
+
+    fn list(&mut self, request: Request<ListRequest>) -> Self::ListFuture {
+        trace!("[LIST] request = {:?}", request);
+        unimplemented!()
+    }
+
+    fn write(&mut self, request: Request<WriteRequest>) -> Self::WriteFuture {
+        trace!("[WRITE] request = {:?}", request);
+        unimplemented!()
+    }
+}
